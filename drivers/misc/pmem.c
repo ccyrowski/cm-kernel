3a0de34424b009eb609b880e766d838ef93014fa
Steve Kondik <shade@toxygene.(none)>
2009-08-11 21:05:49 -0400

    Revert "pmem: Expose is_pmem_file to the in-kernel users."
    This reverts commit 0d1060f749aade019e03fa4400e5174ac27a4c30.

--- drivers/misc/pmem.c
+++ drivers/misc/pmem.c
@@ -186,7 +186,7 @@ static int get_id(struct file *file)
 	return MINOR(file->f_dentry->d_inode->i_rdev);
 }
 
-int is_pmem_file(struct file *file)
+static int is_pmem_file(struct file *file)
 {
 	int id;
 
