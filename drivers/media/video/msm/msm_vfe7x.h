b73400a000075990b35e47343ddeb1818089c1b8
Iliyan Malchev <malchev@google.com>
2009-09-02 18:36:42 -0700

    [ARM] msm: camera: add Code-Aurora license and GPL boilerplate
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- drivers/media/video/msm/msm_vfe7x.h
+++ drivers/media/video/msm/msm_vfe7x.h
@@ -1,6 +1,21 @@
-/*
- * Copyright (C) 2008-2009 QUALCOMM Incorporated.
+/* Copyright (c) 2009, Code Aurora Forum. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
  */
+
 #ifndef __MSM_VFE7X_H__
 #define __MSM_VFE7X_H__
 #include <media/msm_camera.h>
