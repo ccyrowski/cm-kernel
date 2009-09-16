d968d17742813293334126aec3eb32166d9ff3be
Iliyan Malchev <malchev@google.com>
2009-09-16 12:34:14 -0700

    [ARM] msm: camera: use the camera_flash function from the board file
    Also, remove CONFIG_MSM_CAMERA_FLASH.  This is now configured automarically via
    the board file.
    
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- drivers/media/video/msm/msm_camera.c
+++ drivers/media/video/msm/msm_camera.c
@@ -1647,6 +1647,38 @@ static long msm_ioctl_common(struct msm_device *pmsm,
 	}
 }
 
+int msm_camera_flash(struct msm_sync *sync, int level)
+{
+	int flash_level;
+
+	if (!sync->sdata->camera_flash) {
+		pr_err("%s: camera flash is not supported.\n", __func__);
+		return -EINVAL;
+	}
+
+	if (!sync->sdata->num_flash_levels) {
+		pr_err("%s: no flash levels.\n", __func__);
+		return -EINVAL;
+	}
+
+	switch(level) {
+	case MSM_CAMERA_LED_HIGH:
+		flash_level = sync->sdata->num_flash_levels - 1;
+		break;
+	case MSM_CAMERA_LED_LOW:
+		flash_level = sync->sdata->num_flash_levels / 2;
+		break;
+	case MSM_CAMERA_LED_OFF:
+		flash_level = 0;
+		break;
+	default:
+		pr_err("%s: invalid flash level %d.\n", __func__, level);
+		return -EINVAL;
+	}
+
+	return sync->sdata->camera_flash(level);
+}
+
 static long msm_ioctl_config(struct file *filep, unsigned int cmd,
 	unsigned long arg)
 {
@@ -1720,7 +1752,7 @@ static long msm_ioctl_config(struct file *filep, unsigned int cmd,
 			ERR_COPY_FROM_USER();
 			rc = -EFAULT;
 		} else
-			rc = msm_camera_flash_set_led_state(led_state);
+			rc = msm_camera_flash(pmsm->sync, led_state);
 		break;
 	}
 
