6d917a891845b7e237372c013327f59733dd22bc
Steve Kondik <shade@toxygene.(none)>
2009-09-15 21:50:47 -0400

    Apply patchset 2.6.29.6

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -100,6 +100,8 @@ static int neg_one = -1;
 
 static int zero;
 static int __maybe_unused two = 2;
+static int one = 1;
+static int two = 2;
 static unsigned long one_ul = 1;
 static int __read_mostly one = 1;
 static int __read_mostly one_hundred = 100;
@@ -1289,10 +1291,7 @@ static struct ctl_table fs_table[] = {
 		.data		= &lease_break_time,
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
-		.proc_handler	= &proc_dointvec_minmax,
-		.strategy	= &sysctl_intvec,
-		.extra1		= &zero,
-		.extra2		= &two,
+		.proc_handler	= &proc_dointvec,
 	},
 #endif
 #ifdef CONFIG_AIO
@@ -1333,7 +1332,10 @@ static struct ctl_table fs_table[] = {
 		.data		= &suid_dumpable,
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
-		.proc_handler	= &proc_dointvec,
+		.proc_handler	= &proc_dointvec_minmax,
+		.strategy	= &sysctl_intvec,
+		.extra1		= &zero,
+		.extra2		= &two,
 	},
 #if defined(CONFIG_BINFMT_MISC) || defined(CONFIG_BINFMT_MISC_MODULE)
 	{
